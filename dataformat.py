5a1cc0b1a8d30b2eff353a384f17d3b4ab924c4c
Leello Tadesse DADI <leello.dadi@telecom-paristech.fr>
2017-06-18 17:45:18 +0200

    Softmax classification and MNIST data formatting

---
+++ dataformat.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 18 16:31:58 2017
+
+
+"""
+
+'''
+Module qui reutilise les fonctions de lecture de tensorflow pour reformatter les images dans MNIST
+'''
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def down_sample(image, nbits):
+    down_sampled_image = np.zeros_like(image)
+    for i in range(len(image)):
+        #formule pour reduire le nombre de bits, elle marche
+        down_sampled_image[i] = np.floor((image[i] * 255) / (2**(8-nbits)))/(2**nbits-1) 
+    return down_sampled_image
+    
+def get_data(nbits):
+    down_sampled_images = np.zeros_like(mnist.train.images)
+    for i in range(len(down_sampled_images)):
+        down_sampled_images[i] = down_sample(mnist.train.images[i], nbits)
+    return down_sampled_images, mnist.train.labels
---
+++ tensor.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 18 15:11:19 2017
+
+
+"""
+
+import tensorflow as tf
+
+from dataformat import mnist, get_data
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+#model
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+ycorr = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=ycorr, logits=y))
+
+train_step = tf.train.GradientDescentOptimizer(0.95).minimize(cross_entropy)
+
+
+sess = tf.InteractiveSession()
+
+tf.global_variables_initializer().run()
+
+
+batch_xs, batch_ys = get_data(1)
+
+for i in range(550):
+    bxs, bys = batch_xs[100*i:100*(i+1)], batch_ys[100*i:100*(i+1)]
+    sess.run(train_step, feed_dict={x : bxs, ycorr : bys})
+    
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(ycorr, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(sess.run(accuracy, feed_dict={x:mnist.test.images, ycorr: mnist.test.labels}))
